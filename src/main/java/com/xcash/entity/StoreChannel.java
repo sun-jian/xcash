14d26b375e25698322b5ab3af75ef8f2fc0fc1cf
Zaiping Ma <zaiping.ma@blackboard.com>
2017-11-02 11:30:31 +0800

    gateway name

--- src/main/java/com/xcash/entity/StoreChannel.java
+++ src/main/java/com/xcash/entity/StoreChannel.java
@@ -30,6 +30,12 @@ public class StoreChannel {
 			this.paymentGatewayName = "银商C扫B";
 		} else if("JUZHEN".equals(paymentGateway)) {
 			this.paymentGatewayName = "钜真";
+		} else if("CHINAUMSH5".equals(paymentGateway)) {
+			this.paymentGatewayName = "银商H5";
+		} else if("CHINAUMSWAP".equals(paymentGateway)) {
+			this.paymentGatewayName = "银商H5";
+		} else if("UPAY".equals(paymentGateway)) {
+			this.paymentGatewayName = "收钱吧H5";
 		}
 	}public String getPaymentGatewayName() {
 		return paymentGatewayName;
